5d3c3772b92cd547a10b44ad5b20791cac3e0e66
Reinaldo Duarte <reicd1@gmail.com>
2025-09-14 10:39:24 -0300

    Concluído 1º teste de models
    Após alguns erros de execução, exibição correta.

--- sac/urls.py
+++ sac/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from sac.views import index, about
 
+
 #Lista de caminhos (urls) do app sac
 urlpatterns = [
     #path('rota/', view, name='nome_da_rota'),
     #Exemplo:
     path('index/', index, name='sac-index'),
-    path('about/', about, name='sac-about'),    
+    path('about/', about, name='sac-about'),
+   
 ]
--- sac/views.py
+++ sac/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from sac.models import Question
+
 # Create your views here.
 from django.http import HttpResponse
 # Definir uma view baseada em função.
@@ -8,4 +10,8 @@ def index(request):
     return render(request, 'index.html', {'titulo': 'Página Inicial'})
 # Definir uma view baseada em função.
 def about(request):
-    return HttpResponse("This is the about page of the sac app.")
+    # return HttpResponse("This is the about page of the sac app.")
+    question = Question.objects.all()
+    context = {'all_questions': question}
+    return render(request, 'sac/question.html', context)
+
