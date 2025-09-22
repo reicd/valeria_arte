2754bbe98a7d54986366bf893e2e43b654d5b6f9
Reinaldo Duarte <reicd1@gmail.com>
2025-09-22 11:01:10 -0300

    Continuando o trabalho
    Cadastro de usuário completo

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    # You can add additional fields here if needed
+    data_nascimento = models.DateField("Data de Nascimento" 
+    null=True, 
+    blank=True)
+    telefone = models.CharField("Telefone",max_length=15, null=True, blank=True)
+    endereco = models.CharField("Endereço",max_length=255, null=True, blank=True)
+    cidade = models.CharField("Cidade",max_length=100, null=True, blank=True)
+    estado = models.CharField("Estado",max_length=100, null=True, blank=True)
+    pais = models.CharField("País",max_length=100, null=True, blank=True)
+    cep = models.CharField("CEP",max_length=20, null=True, blank=True)
+    cpf = models.CharField("CPF",max_length=14, null=True, blank=True)
