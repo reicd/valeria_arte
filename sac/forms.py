6f60af4752d4e814f37a59db0621999df7869d48
reicd <reicd1@gmail.com>
2025-11-07 15:31:08 -0300

    Corrigido data da edição
    com o uso de ia

--- sac/forms.py
+++ sac/forms.py
@@ -8,9 +8,21 @@ class QuestionForm(forms.ModelForm):
         fields = ('question_text', 'pub_date')
         widgets = {
             'question_text': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-            'pub_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'pub_date': forms.DateTimeInput(
+                attrs={
+                    'class': 'form-control',
+                    'type': 'datetime-local'
+                },
+                format='%Y-%m-%dT%H:%M'
+            ),
         }
         labels = {
             'question_text': 'Pergunta',
             'pub_date': 'Data de Publicação',
         }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk and self.instance.pub_date:
+            # Formata a data para o formato aceito pelo input datetime-local
+            self.initial['pub_date'] = self.instance.pub_date.strftime('%Y-%m-%dT%H:%M')
