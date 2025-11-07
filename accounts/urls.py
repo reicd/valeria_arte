602fe0240a3ceef88686d867be813d064620468b
reicd <reicd1@gmail.com>
2025-11-07 08:36:28 -0300

    foto no terfil
    corrigir r fazer alterar

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from .views import AccountCreateView, AccountUpdateView
 urlpatterns = [
     # Rota para a página de registro de novos usuários
     path('signup/', AccountCreateView.as_view(), name='signup'),
+    # Usar a classe importada diretamente (AccountUpdateView), não 'views.AccountUpdateView'
     path('account/<int:pk>/edit/', AccountUpdateView.as_view(), name='account_edit'),
 ]
