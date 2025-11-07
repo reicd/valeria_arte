5541567fa6fc4414c33aa392fb8536fc272a0880
reicd <reicd1@gmail.com>
2025-11-07 18:49:02 -0300

    fim do dia
    cansado mas falta pouco.

--- sac/views.py
+++ sac/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import DetailView
+from django.views.generic import DetailView,ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from sac.models import Question
@@ -112,3 +112,8 @@ class QuestionDetailView(LoginRequiredMixin, DetailView):
     model = Question
     template_name = 'sac/question_detail.html'
     context_object_name = 'question'
+
+class QuestionListView(LoginRequiredMixin, ListView):
+    model = Question
+    template_name = 'sac/question_list.html'
+    context_object_name = 'questions'
